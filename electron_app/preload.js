104ab475c07d85e7d64cac17441a4e141b7e8870
Eric Zhong <zhongeric08@gmail.com>
2026-05-09 23:08:19 -0400

    Add env example

--- electron_app/preload.js
+++ electron_app/preload.js
@@ -1,7 +1,7 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('api', {
-  getFundamentals: (symbol) => ipcRenderer.invoke('get_fundamentals', symbol),
-  getStockStatsIndicator: (symbol, indicator, currDate, lookBackDays) =>
-    ipcRenderer.invoke('get_stock_stats_indicators_window', symbol, indicator, currDate, lookBackDays)
-});
+// contextBridge.exposeInMainWorld('api', {
+//   getFundamentals: (symbol) => ipcRenderer.invoke('get_fundamentals', symbol),
+//   getStockStatsIndicator: (symbol, indicator, currDate, lookBackDays) =>
+//     ipcRenderer.invoke('get_stock_stats_indicators_window', symbol, indicator, currDate, lookBackDays)
+// });
